48446acc080ea144f19cae36bfcea75ef27a77f1
afomin <afomin@topcon.com>
2018-04-06 11:54:52 +0300

    add with Alex simple javascriptgit statusgit status

--- web_chat/app/__init__.py
+++ web_chat/app/__init__.py
@@ -2,29 +2,77 @@ import flask
 import logging
 
 app = flask.Flask(__name__)
+app.config.from_object("config")
+
+# import app.views
+
 LOG = logging.getLogger(__name__)
 
-posts_history = [
-    {
-        'author': {'nickname': 'Alexander'},
-        'body': "hello, i'am Alexander Fomin"
-    },
-    {
-        'author': {'nickname': 'Alexey'},
-        'body': "Hi, Alexander, what you done for this morning?"
-    },
-    {
-        'author': {'nickname': 'Sergey'},
-        'body': "Hi, guys, i'am also here"
-    }
+# posts = [
+#     {
+#         'author': {'nickname': 'Alexander'},
+#         'body': "hello, i'am Alexander Fomin"
+#     },
+#     {
+#         'author': {'nickname': 'Alexey'},
+#         'body': "Hi, Alexander, what you done for this morning?"
+#     },
+#     {
+#         'author': {'nickname': 'Sergey'},
+#         'body': "Hi, guys, i'am also here"
+#     }
 
-]
+# ]
+
+# $('#test').text("dsds")
+# Object [ h1#test ]
+# setInterval(functon(){})
+# SyntaxError: missing ) after argument list[Learn More] debugger eval code:1:21
+# var counter=0;
+# undefined
+# counter\
+# SyntaxError: illegal character[Learn More] debugger eval code:1:7
+# counter
+# 0
+# setInterval(function(){$('#test').text(''+counter);}, 100);
+# 2
+# setInterval(function(){$('#test').text('' + counter); console.log('xxx');}, 1000);
+# 3
+# xxx
+# debugger eval code:1:55
+# var xxx=12;
+# undefined
+# xxx
+# debugger eval code:1:55
+# setInterval(function(){$('#test').text('' + counter); counter += 1; console.log('xxx');}, 1000);
+my_counter = {"counter": 0}
 
 
 @app.route("/")
+@app.route("/index")
 def test_function():
     LOG.critical("/ called")
     user = {'nickname': 'Alexander'}
+    posts = [
+        {
+            'author': {'nickname': 'Alexander'},
+            'body': "hello, i'am Alexander Fomin"
+        },
+        {
+            'author': {'nickname': 'Alexey'},
+            'body': "Hi, Alexander, what you done for this morning?"
+        },
+        {
+            'author': {'nickname': 'Sergey'},
+            'body': "Hi, guys, i'am also here"
+        }
+    ]
     return flask.render_template(
-        "index.html", user=user, posts_history=posts_history)
+        "index.html", user=user, posts=posts)
 
+
+@app.route("/test")
+def test_function_2():
+    my_counter["counter"] += 1
+    print("test_function_2 called " + str(my_counter["counter"]))
+    return "PRVIET ALEXANDER + " + str(my_counter["counter"])
---
+++ web_chat/app/forms.py
@@ -0,0 +1,7 @@
+# from flask.ext.wtf import Form
+# import wtforms
+
+
+# class LoginForm(Form):
+#     openid = wtforms.TextField('openid', validators=[wtforms.Required])
+#     remember_me = wtforms.BooleanField('remember_me', default=False)
